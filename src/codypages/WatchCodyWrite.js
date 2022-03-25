99eea55d1f5f53f70e44a6cc33ca88e5dce21348
Jaemin Kim <clsne0328@daum.net>
2022-03-25 23:46:15 +0900

    []

--- src/codypages/WatchCodyWrite.js
+++ src/codypages/WatchCodyWrite.js
@@ -1,226 +1,27 @@
-// import React, { useEffect, useState } from "react";
-// import styled from "styled-components";
-// import { InputGroup, FormControl, Form, Button } from "react-bootstrap";
-// import { useDispatch, useSelector } from "react-redux";
-// import StarRating from "../option/StarRating";
-// import { actionCreators as postActions } from "../redux/modules/post";
-// import Upload from "../option/Upload";
-// import { useHistory } from "react-router-dom";
-// const WatchCodyWrite = (props) => {
-//   const token = localStorage.getItem("token");
-//   const history = useHistory();
-//   const dispatch = useDispatch();
-//   const [title, setTitle] = useState("");
-//   const [brand, setBrand] = useState("");
-//   const [model, setModel] = useState("");
-//   const [content, setContent] = useState("");
-//   const [files, setImages] = useState([]);
-//   const [Value, setValue] = useState(0);
-
-//   const formData = new FormData();
-
-//   const changetitle = (e) => {
-//     setTitle(e.target.value);
-//     console.log(e.target.value);
-//   };
-
-//   const changebrand = (e) => {
-//     setBrand(e.target.value);
-//     console.log(e.target.value);
-//   };
-
-//   const changemodel = (e) => {
-//     setModel(e.target.value);
-//     console.log(e.target.value);
-//   };
-
-//   const changeContent = (e) => {
-//     setContent(e.target.value);
-//     console.log(e.target.value);
-//   };
-
-//   const updateImages = (newImages) => {
-//     setImages(newImages);
-//   };
-
-//   const changeValue = () => {
-//     setValue("");
-//   };
-//   const submitHandler = (e) => {
-//     e.preventDefault();
-
-//     if (!title || !brand || !model || !content || !files || !Value) {
-//       return alert("모든 값을 넣어주셔야 합니다.");
-//     }
-
-//     formData.append("title", title);
-//     formData.append("brand", brand);
-//     formData.append("model", model);
-//     formData.append("content", content);
-//     formData.append("multipartFile", files);
-//     formData.append("Value", Value);
-
-//     console.log(Value);
-//     dispatch(postActions.addPostFB(title, brand, model, content, files, Value));
-//   };
-//   return (
-//     <div>
-//       <WatchCodyWriteBlock>
-//         <div className="container">
-//           <div className="flex-box">
-//             <Upload />
-
-//             <InputGroup className="mb-3" style={{ height: "50px" }}>
-//               <InputGroup.Text id="basic-addon1" style={{ width: "80px" }}>
-//                 <span style={{ marginLeft: "11px" }}>제목</span>
-//               </InputGroup.Text>
-//               <FormControl
-//                 onChange={changetitle}
-//                 value={title}
-//                 aria-label="Username"
-//                 aria-describedby="basic-addon1"
-//               />
-//             </InputGroup>
-
-//             <InputGroup className="mb-3" style={{ height: "50px" }}>
-//               <InputGroup.Text id="basic-addon1" style={{ width: "80px" }}>
-//                 <span style={{ marginLeft: "7px" }}>브랜드</span>
-//               </InputGroup.Text>
-//               <FormControl
-//                 onChange={changebrand}
-//                 value={brand}
-//                 aria-label="Username"
-//                 aria-describedby="basic-addon1"
-//               />
-//             </InputGroup>
-
-//             <InputGroup className="mb-3" style={{ height: "50px" }}>
-//               <InputGroup.Text id="basic-addon1" style={{ width: "80px" }}>
-//                 <span style={{ marginLeft: "11px" }}>모델</span>
-//               </InputGroup.Text>
-//               <FormControl
-//                 onChange={changemodel}
-//                 value={model}
-//                 aria-label="Username"
-//                 aria-describedby="basic-addon1"
-//               />
-//             </InputGroup>
-
-//             <InputGroup className="mb-3" style={{ height: "300px" }}>
-//               <FormControl
-//                 onChange={changeContent}
-//                 value={content}
-//                 placeholder="내용"
-//                 aria-label="Username"
-//                 aria-describedby="basic-addon1"
-//               />
-//             </InputGroup>
-
-//             <div className="lb-star">
-//               <h5 className="lb-text">평점</h5>
-//               <StarRating value={Value} changeValue={setValue} />
-//             </div>
-//             <Button
-//               onClick={submitHandler}
-//               className="lb-button"
-//               variant="light"
-//             >
-//               등록하기
-//             </Button>
-//           </div>
-//         </div>
-//         <div className="container-test">
-//           <div className="flex-box-test">
-//             <input type="text" />
-//             <input type="text" />
-//             <input type="text" />
-//             <input type="text" />
-//             <input type="text" />
-//             <input type="text" />
-//           </div>
-//           <button type="text">등록하기</button>
-//         </div>
-//       </WatchCodyWriteBlock>
-//     </div>
-//   );
-// };
-
-// const WatchCodyWriteBlock = styled.div`
-//   .container {
-//   }
-//   .flex-box {
-//     display: flex;
-//     width: 80%;
-//     margin: auto;
-//     flex-direction: column;
-//     height: 100vh;
-//     align-items: center;
-//     justify-content: center;
-//   }
-//   .lb-text {
-//     display: flex;
-//     font-size: 35px;
-
-//     padding: 10px 30px;
-//     border-radius: 10px;
-//     background-color: rgb(233, 236, 239);
-//     margin-inline-end: auto;
-//   }
-//   .lb-button {
-//     padding: 10px 20px;
-//     border-radius: 10px;
-//     background-color: #e0e0e0;
-//     width: 30%;
-//     height: 7vh;
-//     margin-top: 50px;
-//     cursor: pointer;
-//     :hover {
-//       opacity: 0.3;
-//       transition: all 500ms;
-//     }
-//   }
-//   .lb-star {
-//     display: flex;
-//     margin-inline-end: auto;
-//   }
-//   .container-test {
-//     display: flex;
-//     flex-direction: column;
-//     width: 500px;
-//     height: 300px;
-//     margin: auto;
-//   }
-//   .flex-box {
-//     width: 300px;
-//     height: 200px;
-//   }
-// `;
-// export default WatchCodyWrite;
 import axios from "axios";
 import React, { useState } from "react";
 import styled from "styled-components";
 function WatchCodyWrite() {
-  const [imgFile, setImgFile] = useState("");
-
-  const handleChangeFile = (fileBlob) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(fileBlob);
-    // reader 가 성공적으로 파일을 읽었을때 , 트리거되는 이벤트핸들러
-    // 내부에 이미지 프리뷰 로직
-    return new Promise((resolve) => {
-      reader.onload = () => {
-        setImgFile(reader.result);
-        resolve();
-      };
-    });
-  };
-
   const [a, setA] = useState();
   const [b, setB] = useState();
   const [c, setC] = useState();
   const [d, setD] = useState();
   const [e, setE] = useState();
+  const [myImage, setMyImage] = useState([]);
 
+  let list;
+  const addImage = (e) => {
+    list = e.target.files;
+
+    const URLlist = [...myImage];
+    for (let i = 0; i < list.length; i += 1) {
+      const showImage = URL.createObjectURL(list[i]);
+
+      URLlist.push(showImage);
+      list.push({ list });
+    }
+    setMyImage(URLlist);
+  };
   const onButton = (e) => {
     setA(e.target.value);
   };
@@ -239,7 +40,7 @@ function WatchCodyWrite() {
 
   const subMit = (e) => {
     e.preventDefault();
-
+    console.log(list);
     console.log(a, b, c, d, e);
   };
 
@@ -254,17 +55,15 @@ function WatchCodyWrite() {
       <WatchCodyWriteBlock>
         <div className="container-test">
           <div className="flex-box-test">
-            <input
-              type="file"
-              accept="image/*"
-              onChange={(e) => {
-                handleChangeFile(e.target.flies[0]);
-              }}
-              style={{ marginTop: "30px" }}
-            />
-            <div className="flex-box-test">
-              <div> {imgFile && <img src={imgFile} alt="preview-img" />}</div>
-            </div>
+            <label htmlFor="input-file" onChange={addImage}>
+              <input
+                style={{ marginTop: "20px" }}
+                type="file"
+                accept="image/*"
+                multiple="multiple"
+                id="input-file"
+              />
+            </label>
             {/* 특정 미디어 유형의 모든 파일을 사용할 수 있도록 하려면 accept="image/*" */}
             <input type="text" value={a} onChange={onButton} />
             <input type="text" value={b} onChange={onButton2} />
@@ -281,11 +80,5 @@ function WatchCodyWrite() {
   );
 }
 
-const WatchCodyWriteBlock = styled.div`
-  .flex-box-test {
-    background-color: grey;
-    width: 150px;
-    height: 150px;
-  }
-`;
+const WatchCodyWriteBlock = styled.div``;
 export default WatchCodyWrite;
