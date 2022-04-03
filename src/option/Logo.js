4b8e79dccbbf114ee4a5d1d56683fc74d22d91ea
김형래 <khr157929@gmail.com>
2022-04-03 12:57:51 +0900

    logo

--- src/option/Logo.js
+++ src/option/Logo.js
@@ -1,40 +1,70 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import MainLogo from "../images/logo3.jpg";
 import Lock from "../svg/Lock.svg";
 import ShoppingCart from "../svg/ShoppingCart.svg";
 import User from "../svg/User.svg";
+import "../App.css";
 
 const Logo = () => {
   return (
-    <div>
-      <LogoBlock>
-        <div className="logo">
-          <img src={MainLogo} alt="logo3" />
-        </div>
-        <div className="flex-item">
-          <img style={{ marginRight: "10px" }} src={Lock} alt="logo3" />
-          <p>LOGIN</p>
-          <img style={{ marginRight: "10px" }} src={ShoppingCart} alt="logo3" />
-          <p>CART</p>
-          <img style={{ marginRight: "10px" }} src={User} alt="logo3" />
-          <p>MY PAGE</p>
-        </div>
-      </LogoBlock>
+    <div className="wrap">
+      <div className="center">
+        <LogoBlock>
+          <img src={MainLogo} alt="logoImg" className="logo" />
+          <div className="btn-wrap">
+            <Link to="/login">
+              <img src={Lock} alt="loginImg" />
+              <button type="button">LOGIN</button>
+            </Link>
+            <Link to="/login">
+              <img src={ShoppingCart} alt="loginImg" />
+              <button type="button">CART</button>
+            </Link>
+            <Link to="/login">
+              <img src={User} alt="loginImg" />
+              <button type="button">SIGN UP</button>
+            </Link>
+          </div>
+        </LogoBlock>
+      </div>
     </div>
   );
 };
 const LogoBlock = styled.div`
   display: flex;
   justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 10rem;
 
-  .logo {
-    margin-left: 43em;
-  }
-  .flex-item {
+  .btn-wrap {
+    position: absolute;
     display: flex;
-    padding: 40px;
-    margin-left: 20em;
+    align-items: center;
+    right: 2rem;
+    a {
+      width: 6rem;
+      height: 4rem;
+      text-align: center;
+      img {
+        margin: auto;
+      }
+
+      button {
+        border: transparent;
+        font-size: 1.4rem;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    .logo {
+      position: absolute;
+      left: -10%;
+    }
   }
 `;
+
 export default Logo;
